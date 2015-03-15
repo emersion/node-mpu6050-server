798ffbee3c1c62c3ed71a09133ce96d6c0e933a7
emersion <contact@emersion.fr>
2015-03-15 10:03:49 +0000

    Now using async methods

--- index.js
+++ index.js
@@ -10,48 +10,58 @@ var sensor = new MPU6050(i2c1, address);
 var app = express();
 var appWs = expressWs(app);
 
-app.ws('/socket', function (ws, res) {});
+var reading = false;
+app.ws('/socket', function (ws, res) {
+	if (!reading) {
+		readSensor();
+	}
+});
 var wss = appWs.getWss('/socket');
 
-app.get('/rotation', function (req, res) {
-	var rot;
-	try {
-		rot = sensor.readRotationSync();
-	} catch (e) {
-		console.log(e);
-		res.status(500).send(e);
+var readSensor = function () {
+	if (!wss.clients.length) {
+		reading = false;
 		return;
 	}
+	if (!reading) {
+		reading = true;
+	}
 
-	res.send(rot.x+' '+rot.y);
+	var start = new Date().getTime();
+	sensor.read(function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			var time = new Date().getTime() - start;
+			var msg = JSON.stringify(data);
+			wss.clients.forEach(function (client) {
+				try {
+					client.send(msg);
+				} catch (e) {
+					console.log(e);
+				}
+			});
+			readSensor();
+		}
+	});
+};
+
+app.get('/rotation', function (req, res) {
+	sensor.readRotation(function (err, rot) {
+		if (err) {
+			console.log(e);
+			res.status(500).send(e);
+			return;
+		}
+
+		res.send(rot.x+' '+rot.y);
+	});
 });
 
 app.use(express.static(__dirname+'/public'));
 
 var server = app.listen(process.env.PORT || 9000, function () {
 	console.log('Server listening', server.address());
-
-	setInterval(function () {
-		if (!wss.clients.length) {
-			return;
-		}
-
-		var data;
-		try {
-			data = sensor.readSync();
-		} catch (e) {
-			console.log(e);
-			return;
-		}
-		var msg = JSON.stringify(data);
-		wss.clients.forEach(function (client) {
-			try {
-				client.send(msg);
-			} catch (e) {
-				console.log(e);
-			}
-		});
-	}, 250);
 });
 
 module.exports = server;
